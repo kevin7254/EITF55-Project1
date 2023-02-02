9746b9ad7271e5c4fef930e90cf0819bb6054b9d
Viktor Eriksson <viktor.eriksson.privat@gmail.com>
2023-02-02 11:13:26 +0100

    test

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-
-        RabinMiller rabinMiller = new RabinMiller(1);
-    }
+     
+        RabinMiller rabinMiller = new RabinMiller();
+    
+        for (int i = 0; i < 20; i++) {// assignment 2.1: 20 
+            rabinMiller.checkIfComposite(561);
+        }
+}
 }
--- src/RabinMiller.java
+++ src/RabinMiller.java
@@ -1,19 +1,14 @@
 public class RabinMiller {
-    private int iterations;
 
-    public RabinMiller(int iterations) {
-        this.iterations = iterations;
-
-        boolean s = checkIfComposite(561);
-        System.out.println(s);
+    public RabinMiller() {
     }
 
-    public boolean checkIfComposite(int n) { //Composite = not prime
+    public boolean checkIfComposite(int n) { // Composite = not prime
         int x;
         if (n > 3 && (n % 2 != 0)) {
-            int r = 0;
-            int s = (n - 1); //Write n-1 = 2^0 * s
-            while(s % 2 == 0) {
+            int r = 0; // 2^0
+            int s = (n - 1); // write n-1 = 2^0 * s
+            while(s % 2 == 0) {// check if s % 2 ==0, increment r to find out the smallest factor s
                 r++;
                 s = s / 2;
             }
@@ -26,7 +21,7 @@ public class RabinMiller {
             x = (int) Math.pow(a, s) % n;
             System.out.println("x = " + x);
 
-            if(x == 1 || x == (n - 1)) return false; // Probably Prime.
+            if(x == 1 || x == (n - 1)) return false; // Probably Prime
 
             for(int j = 1; j<= (r-1);j++) {
                 x = (int) Math.pow(x, 2) % n;
