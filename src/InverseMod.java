361318bb8851070926fc19076f7b5c46a3dd6af8
Viktor Eriksson <viktor.eriksson.privat@gmail.com>
2023-02-02 15:50:07 +0100

    Assignment 4,5 done§

--- src/InverseMod.java
+++ src/InverseMod.java
@@ -1,2 +1,44 @@
+import java.math.BigInteger;
+
 public class InverseMod {
+
+    public InverseMod(){
+       
+    }
+
+
+    
+    public  BigInteger algorithm (BigInteger a, BigInteger m){
+        System.out.println(a + " " + m);
+        BigInteger v2 =  new BigInteger("1");
+        BigInteger v1 =  new BigInteger("0");
+        BigInteger d1 = m;
+        BigInteger d2 =a;
+        BigInteger q, t2,t3, v, d;
+
+
+        
+        while (!d2.equals(BigInteger.ZERO)) {
+            q = d1.divide(d2);
+            t2 = v1.subtract(q.multiply(v2));
+            t3 = d1.subtract(q.multiply(d2));
+            v1 = v2; d1 = d2;
+            v2 = t2; d2 = t3;   
+        }
+        if (v1.compareTo(BigInteger.ZERO) < 0) {
+            v1 = v1.add(m);
+        }
+        
+        
+        v=v1;
+        d=d1;
+
+        System.out.println("d is: " + d +" \nv is: " + v);
+        if (d.equals("1")){
+            return d;
+        }
+        return d;
+    }
 }
+
+
--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,11 @@ public class Main {
         System.out.println("Hello world!");
      
         RabinMiller rabinMiller = new RabinMiller();
+        InverseMod inverse = new InverseMod();
         //assignment2_1(rabinMiller);
         //assignment2_2(rabinMiller);
-        assignment3(rabinMiller);
+        //assignment3(rabinMiller);
+        assignment4(inverse);
 
 
         //Print time in milliseconds
@@ -46,5 +48,19 @@ public class Main {
                 i++;
             }
         }
+    }
+
+
+    public static void assignment4(InverseMod inverse){
+            System.out.println(inverse.algorithm(new BigInteger("5"), new BigInteger("2")));
+            System.out.println(inverse.algorithm(new BigInteger("8"), new BigInteger("5")));
+            System.out.println(inverse.algorithm(new BigInteger("15"), new BigInteger("9")));
+
+
+            System.out.println(inverse.algorithm(new BigInteger("8396256305097038999635256212089884981530394099906140493350945987024380991475879049687351465586141395239672922402620271122951794002504747423174474757914887"),
+             new BigInteger("65537")));
+            System.out.println(inverse.algorithm(new BigInteger("2896660929138039314506773379106703725808659510273801341600541083601687547847610760020351611132508924296522771588376198104627460847204831488292757187828521"),
+             new BigInteger("65537")));
+
     }
 }
